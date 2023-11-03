f5cfe29791f31889db9452a44792b835d2f9324d
junhyeongbak <dj043045@gmail.com>
2023-11-03 13:08:57 +0900

    baekjoon 1-6 / 1-8

---
+++ Baek10869/Baek10926/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baek10926
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string A = "??!";
+
+            Console.WriteLine(input + A);
+     
+        }
+    }
+}
---
+++ Baek10869/Baek18108/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baek18108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int year = Int32.Parse(input);
+            int butaYear = 543;
+
+            Console.WriteLine(year - butaYear);
+           
+        }
+    }
+}
