b7b6bc40e93296338fea395103618f6b7876052a
junhyeongbak <dj043045@gmail.com>
2023-11-03 09:14:58 +0900

    10869

---
+++ Baek10869/Baek10869/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baek10869
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arr = input.Split();
+
+            int A = Convert.ToInt32(arr[0]);
+            int B = Convert.ToInt32(arr[1]);
+
+            if(A > 0 && B < 10000)
+            {
+                Console.WriteLine(A + B);
+                Console.WriteLine(A - B);
+                Console.WriteLine(A * B);
+                Console.WriteLine(A / B);
+                Console.WriteLine(A % B);
+            }
+        }
+    }
+}
