d4833bccded7bbec4376baed23d280c31c69ce5c
junhyeongbak <dj043045@gmail.com>
2023-11-07 08:58:30 +0900

    백준 2588 문제

---
+++ Baek2588/Baek2588/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Baek2588
+{
+    //int num1 =  B -( (B / 10) * 10 );
+    //int num2 = (B - (B / 100) * 100) / 10;
+    //int num3 = B / 100;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(A * (B % 10) );
+            Console.WriteLine(A * ((B % 100) / 10) );
+            Console.WriteLine(A * (B / 100));
+            Console.WriteLine(A * B);
+
+
+        }
+    }
+}
