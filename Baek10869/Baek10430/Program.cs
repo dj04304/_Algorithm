a5067565ffecaadef8fa1560eacbd6d9be2e9285
junhyeongbak <dj043045@gmail.com>
2023-11-06 10:40:13 +0900

    Baek 10430

---
+++ Baek10869/Baek10430/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baek10430
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] arr = input.Split();
+
+            int A = Int32.Parse(arr[0]);
+            int B = Int32.Parse(arr[1]);
+            int C = Int32.Parse(arr[2]);
+
+            int result1 = (A + B) % C;
+            int result2 = ((A % C) + (B % C)) % C;
+            int result3 = (A * B) % C;
+            int result4 = ((A % C) * (B % C)) % C;
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.WriteLine(result4);
+
+        }
+    }
+}
