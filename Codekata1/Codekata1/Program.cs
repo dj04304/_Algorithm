9199e2f6540940bd598a22bf7b9d91738804d365
junhyeongbak <dj043045@gmail.com>
2023-11-15 16:22:03 +0900

    codeKata 1

---
+++ Codekata1/Codekata1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Codekata1
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public string solution(int num)
+            {
+                string answer = "";
+
+                if(num % 2 == 0)
+                {
+                    answer = "Even";
+                }
+                else
+                {
+                    answer = "Odd";
+                }
+
+                return answer;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            
+            int input = int.Parse(Console.ReadLine());
+
+            solution.solution(input);
+
+            string result = solution.solution(input);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
