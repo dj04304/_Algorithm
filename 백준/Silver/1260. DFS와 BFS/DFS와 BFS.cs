a3b3c6ae4174dfcd1211f0881c7b053e956d4bc1
junhyeongBak <105491519+dj04304@users.noreply.github.com>
2024-06-05 14:36:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 84 ms, Memory: 7780 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cs
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cs
@@ -1,28 +1,25 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 
 class Program
 {
-    static int[] n;
-    static List<int>[] graph;
-    static bool[] isDFSVisited;
+    static int[] n = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+    static List<int>[] graph = new List<int>[n[0] + 1];
+    static bool[] isDFSVisited = new bool[n[0] + 1];
     static StringBuilder DFSsb = new StringBuilder();
     static StringBuilder BFSsb = new StringBuilder();
 
+    static int pair = n[1]; 
+
     static void Main(string[] args)
     {
-        n = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-        graph = new List<int>[n[0] + 1];
-        isDFSVisited = new bool[n[0] + 1];
-        
         for (int i = 1; i <= n[0]; i++)
         {
             graph[i] = new List<int>();
         }
 
-        for (int i = 0; i < n[1]; i++)
+        for (int i = 0; i < pair; i++)
         {
             string[] inputs = Console.ReadLine().Split(' ');
             int a = int.Parse(inputs[0]);
@@ -37,10 +34,10 @@ class Program
         }
 
         DFS(n[2]);
-        Console.WriteLine(DFSsb.ToString().Trim());
+        Console.WriteLine(DFSsb.ToString());
 
         BFS(n[2]);
-        Console.WriteLine(BFSsb.ToString().Trim());
+        Console.WriteLine(BFSsb.ToString());
     }
 
     public static void DFS(int now)
@@ -50,33 +47,37 @@ class Program
 
         foreach (int next in graph[now])
         {
-            if (!isDFSVisited[next])
-            {
-                DFS(next);
-            }
+            if (isDFSVisited[next])
+                continue;
+
+            DFS(next);
         }
     }
 
     public static void BFS(int start)
     {
         bool[] isVisited = new bool[n[0] + 1];
+
         Queue<int> queue = new Queue<int>();
         queue.Enqueue(start);
         isVisited[start] = true;
 
-        while (queue.Count > 0)
+        while(queue.Count > 0)
         {
             int now = queue.Dequeue();
             BFSsb.Append(now).Append(' ');
 
             foreach (int next in graph[now])
             {
-                if (!isVisited[next])
-                {
-                    queue.Enqueue(next);
-                    isVisited[next] = true;
-                }
+                if (isVisited[next])
+                    continue;
+
+                queue.Enqueue(next);
+                isVisited[next] = true;
+
             }
         }
+
     }
+
 }
