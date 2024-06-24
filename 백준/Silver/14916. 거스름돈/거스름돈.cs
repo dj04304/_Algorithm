f964759b86a3bbbd650a3241be7867617e43708a
junhyeongBak <105491519+dj04304@users.noreply.github.com>
2024-06-24 14:31:18 +0900

    [Silver V] Title: 거스름돈, Time: 68 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        int input =int.Parse(Console.ReadLine());
+
+        int count = -1;
+
+        if (input % 5 == 0)
+            count = input / 5;
+
+        for (int i = input / 5; i >= 0; i--)
+        {
+            int a = input - (i * 5);
+
+            if (a % 2 == 0)
+            {
+               count =i +( a / 2 );
+               break;
+            }
+        }
+
+        Console.WriteLine(count);
+
+    }
+
+    
+}
