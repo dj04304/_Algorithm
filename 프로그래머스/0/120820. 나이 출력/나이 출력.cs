68de85fe1457531bf9514243648e79502b8a0748
junhyeongBak <105491519+dj04304@users.noreply.github.com>
2025-01-14 14:33:27 +0900

    [level 0] Title: 나이 출력, Time: 0.20 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        
+        int year = 2022;
+        
+        answer = (year + 1) - age;
+        
+        return answer;
+    }
+}
