0316b64659557a6f52f9582cc0f490e2e970073e
junhyeongBak <105491519+dj04304@users.noreply.github.com>
2025-05-20 15:41:56 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.31 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+        public int solution(string my_string, string target)
+    {
+        int answer = 0;
+
+        if(my_string.Contains(target))
+            answer = 1;
+        else
+            answer = 0;
+
+        return answer;
+    }
+}
