0a706339121a0c8b5dd05dcd6ac0cef97debd204
junhyeongBak <105491519+dj04304@users.noreply.github.com>
2025-07-04 15:26:52 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.42 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cs
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cs
@@ -1,16 +1,15 @@
-using System;
-        public class Solution
-        {
-            public bool solution(string s)
-            {
-                bool answer = false;
-
-               if(Int32.TryParse(s, out int result) && (s.Length == 4 || s.Length ==6) )
-                {
-                    return answer = true;
-                }
-
-
-                return answer;
-            }
+public class Solution {
+    public bool solution(string s) {
+        bool answer = true;
+        
+        if(s.Length != 4 && s.Length != 6)
+            return false;
+            
+        foreach(char c in s){
+            if(!char.IsDigit(c))
+                return false;
         }
+        
+        return answer;
+    }
+}
